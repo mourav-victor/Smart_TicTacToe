e8aa7214a25ab3182ddca7bc5160f7cbcc6335bf
mourav-victor <moura.victor14@gmail.com>
2021-07-01 11:35:25 +0200

    adding third IA based on position score

---
+++ tictactoeSP.py
@@ -0,0 +1,16 @@
+## Development and study of a Tic Tac Toe IA 
+
+#######################################################################
+## There will be trheeo IA-based programs to play it:                ##
+##  i - Pontuation based of the number of winner positions           ##
+##  ii - Pontuation based on a weight of winner and loser positions  ##
+##  iii - Pontuation based on a history score of positions           ##
+#######################################################################
+
+import random # randrange and choice modules
+import numpy as np
+
+print("\n\nWelcome to Tic Tac Toe bot, lets go!")
+print("coming soon...")
+
+
